97875890cd44f542aca6e1d33bcf2596ad6131e2
Samuel Lima <105258182+Samuelrlm@users.noreply.github.com>
2025-05-07 21:13:50 -0300

    Update connection.js

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,7 +1,7 @@
 const {Pool} = require('pg')
 
 const connection = new Pool({
-    connectionString: "postgresql://postgres:exDoOVqPmCWFlcmFszFDNBSjELsRlqyN@nozomi.proxy.rlwy.net:35141/railway"
+    connectionString: ""
 })
 
 module.exports = connection
